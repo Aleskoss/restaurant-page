63987132457a8522031f88554947df4b8bc0a434
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-06-29 16:26:51 +0200

    Create button click logic to load correct content

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,21 @@
 import "../styles/style.css"
 import { InitialPageLoad } from "./itinial-page-load"
 
-InitialPageLoad.initialPageLoad()
+document.addEventListener('click', event => {
+  const content = document.querySelector('#content')
+  const homeBtn = document.querySelector('#home-btn')
+  function removeContent(){
+    while(content.firstChild){
+        content.removeChild(content.firstChild)
+    }
+  }
+  console.log(homeBtn.firstChild)
+  let target = event.target.id
+  removeContent()
+  switch(target){
+    case 'home-btn':
+      InitialPageLoad.initialPageLoad()
+      homeBtn.firstChild.classList.toggle("hidden-element")
+      break;
+  }
+})
